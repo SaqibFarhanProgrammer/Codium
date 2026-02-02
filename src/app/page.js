e59e75a16fd480b9c37693408934ad516efb9493
shahzaib887 <shahzaibshazzo@gmail.com>
2026-02-02 13:22:00 +0500

    feat: remove unused imports from page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,6 @@
 import HomePage from '@/components/HomePage';
 import Navbar from '@/components/Navbar';
 import React from 'react';
-import { islogin } from '../../appConfig';
-import { redirect } from 'next/navigation';
-import connectDB from '@/lib/db';
-import uploadOnCloudinery from '@/helpers/uploadOnCloudinery.helper';
 function page() {
   return (
     <div className="">
